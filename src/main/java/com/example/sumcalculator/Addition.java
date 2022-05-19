d21091b654c2c6c7f5ce16158e275096ac260009
aoezalti <ayse.oezaltin@outlook.com>
2022-05-19 20:52:50 +0200

    added Controller & annotations

--- src/main/java/com/example/sumcalculator/Addition.java
+++ src/main/java/com/example/sumcalculator/Addition.java
@@ -1,6 +1,16 @@
 package com.example.sumcalculator;
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
 
 public class Addition {
+
+    private int a;
+    private int b;
+    private Long id;
 
     public int addition(int a, int b){
         int Summe = a + b;
@@ -21,5 +31,14 @@ public class Addition {
         }
 
         return Summe;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
     }
 }
---
+++ src/main/java/com/example/sumcalculator/CalculatorController.java
@@ -0,0 +1,9 @@
+package com.example.sumcalculator;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CalculatorController {
+
+
+}

de2623a87ca1ba161ee1c08dc075c4e4b709c98f
Robert <wi21b059@technikum-wien.at>
2022-05-19 20:44:44 +0200

    added addition for array

--- src/main/java/com/example/sumcalculator/Addition.java
+++ src/main/java/com/example/sumcalculator/Addition.java
@@ -2,7 +2,24 @@ package com.example.sumcalculator;
 
 public class Addition {
 
-    public int addition(int a){
-        return 10;
+    public int addition(int a, int b){
+        int Summe = a + b;
+        return Summe;
+    }
+
+    public double addition(double a, double b){
+        double Sum = a + b;
+        return Sum;
+    }
+
+    public double addition(double[] a){
+        int len = a.length;
+        double Summe = 0;
+
+        for (int i = 0; i < len; i++) {
+            Summe += a[i];
+        }
+
+        return Summe;
     }
 }
